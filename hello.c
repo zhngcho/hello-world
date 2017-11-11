575b773517db1908924a97d706eb3fb1cdbd389c
zhngcho <zhngcho@yeah.net>
2017-11-11 11:38:47 +0800

    添加hello.c

---
+++ hello.c
@@ -0,0 +1,27 @@
+/*************************************************************************
+*    > File Name: hello.c
+*    > Author: Socra
+*    > Company: Babylon 
+*    > Created Time: 2017年11月11日 星期六 11时33分50秒
+*************************************************************************/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <pthread.h>
+#include <sys/socket.h>
+
+
+int main(int argc,char *argv[])
+{
+    printf("hello world!");
+    
+    
+    return 0;
+}
